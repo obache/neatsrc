528fa6fa16eb07c55a92d173c1b459b15e9f904d
OBATA Akio <obache@outlook.com>
2015-06-23 11:31:23 +0900

    Change overridng non-existent file to ERROR
    It is usually happend with upstream file layout changes or renaming.
    Such missing catch-up should be detected obviously.

--- mk/subst.mk
+++ mk/subst.mk
@@ -115,7 +115,7 @@ ${_SUBST_COOKIE.${_class_}}:
 		case $$file in /*) ;; *) file="./$$file";; esac;	\
 		tmpfile="$$file"${_SUBST_BACKUP_SUFFIX:Q};		\
 		if [ ! -f "$$file" ]; then				\
-			${WARNING_MSG} "[subst.mk:${_class_}] Ignoring non-existent file \"$$file\"."; \
+			${ERROR_MSG} "[subst.mk:${_class_}] non-existent file \"$$file\"."; \
 		elif ${_SUBST_IS_TEXT_FILE.${_class_}}; then		\
 			${MV} -f "$$file" "$$tmpfile" || exit 1;	\
 			${SUBST_FILTER_CMD.${_class_}}			\
