093283619d5c112388f03b67354246763c70506f
OBATA Akio <obache@outlook.com>
2015-06-27 13:07:15 +0900

    Fix potential null pointer dereference

--- sysutils/caja/patches/patch-libcaja-private_caja-undostack-manager.c
+++ sysutils/caja/patches/patch-libcaja-private_caja-undostack-manager.c
@@ -1,14 +1,24 @@
 $NetBSD$
 
-* stdlib.h is required for malloc(3)
+* Use g_malloc instead of malloc to be safe
 
 --- libcaja-private/caja-undostack-manager.c.orig	2015-06-11 20:59:47.000000000 +0000
 +++ libcaja-private/caja-undostack-manager.c
-@@ -33,6 +33,7 @@
- #include <glib/gi18n.h>
- #include <locale.h>
- #include <gdk/gdk.h>
-+#include <stdlib.h>
+@@ -903,7 +903,7 @@ caja_undostack_manager_data_add_trashed_
+     return;
  
- /* *****************************************************************
-  Private fields
+   guint64 *modificationTime;
+-  modificationTime = (guint64 *) malloc (sizeof (guint64));
++  modificationTime = (guint64 *) g_malloc (sizeof (guint64));
+   *modificationTime = mtime;
+ 
+   char *originalURI = g_file_get_uri (file);
+@@ -924,7 +924,7 @@ void caja_undostack_manager_data_add_fil
+     return;
+ 
+   guint32 *currentPermission;
+-  currentPermission = (guint32 *) malloc (sizeof (guint32));
++  currentPermission = (guint32 *) g_malloc (sizeof (guint32));
+   *currentPermission = permission;
+ 
+   char *originalURI = g_file_get_uri (file);
