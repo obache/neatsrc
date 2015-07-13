0de1f535cbd54ae37b043b44e3587f5254d4ee29
OBATA Akio <obache@outlook.com>
2015-07-13 18:52:49 +0900

    Import sunpinyin version 2.0.3

---
+++ inputmethod/sunpinyin/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+#
+
+BUILDLINK_TREE+=	sunpinyin
+
+.if !defined(SUNPINYIN_BUILDLINK3_MK)
+SUNPINYIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.sunpinyin+=	sunpinyin>=2.0.3
+BUILDLINK_PKGSRCDIR.sunpinyin?=	../../inputmethod/sunpinyin
+
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.endif	# SUNPINYIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-sunpinyin
