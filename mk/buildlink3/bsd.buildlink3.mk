0919bd604dbb40da57d54f98d4e110c7c00e30bb
OBATA Akio <obache@outlook.com>
2015-07-14 18:36:18 +0900

    Use pkg_info -qL to retrieve files of the buildlink package

--- mk/buildlink3/bsd.buildlink3.mk
+++ mk/buildlink3/bsd.buildlink3.mk
@@ -624,10 +624,10 @@ buildlink-${_pkg_}-cookie:
 
 BUILDLINK_CONTENTS_FILTER.${_pkg_}?=					\
 	${EGREP} '(include.*/|\.h$$|\.idl$$|\.pc$$|/lib[^/]*\.[^/]*$$)'
-# XXX: Why not pkg_info -qL?
+
 BUILDLINK_FILES_CMD.${_pkg_}?=						\
-	${_BLNK_PKG_INFO.${_pkg_}} -f ${BUILDLINK_PKGNAME.${_pkg_}} |	\
-	${SED} -n '/File:/s/^[ 	]*File:[ 	]*//p' |		\
+	${_BLNK_PKG_INFO.${_pkg_}} -qL ${BUILDLINK_PKGNAME.${_pkg_}} |	\
+	${SED} -e 's,^${BUILDLINK_PREFIX.${_pkg_}}/,,' |		\
 	${BUILDLINK_CONTENTS_FILTER.${_pkg_}} | ${CAT}
 
 # _BLNK_FILES_CMD.<pkg> combines BUILDLINK_FILES_CMD.<pkg> and
