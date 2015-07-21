0c190684cedce963119c5e4108ed85a8845ddf31
OBATA Akio <obache@outlook.com>
2015-07-21 15:42:54 +0900

    Update sunpinyin to pre 2.0.4 release snapshot 20150706

--- inputmethod/sunpinyin/buildlink3.mk
+++ inputmethod/sunpinyin/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	sunpinyin
 .if !defined(SUNPINYIN_BUILDLINK3_MK)
 SUNPINYIN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.sunpinyin+=	sunpinyin>=2.0.3
+BUILDLINK_API_DEPENDS.sunpinyin+=	sunpinyin>=2.0.4beta0
 BUILDLINK_PKGSRCDIR.sunpinyin?=	../../inputmethod/sunpinyin
 
 .include "../../converters/libiconv/buildlink3.mk"
