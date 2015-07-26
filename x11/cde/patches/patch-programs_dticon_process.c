2522283e85e308bb6e2accd198d68d1465d4944b
OBATA Akio <obache@outlook.com>
2015-07-26 18:47:08 +0900

    Better fix to avoid conflict of abs definition

--- x11/cde/patches/patch-programs_dticon_process.c
+++ x11/cde/patches/patch-programs_dticon_process.c
@@ -2,11 +2,11 @@ $NetBSD$
 
 --- programs/dticon/process.c.orig	2015-05-09 23:09:11.000000000 +0000
 +++ programs/dticon/process.c
-@@ -84,6 +84,7 @@
+@@ -82,6 +82,7 @@
+ #include <Xm/Xm.h>
+ #include <Xm/XmP.h>
  #include <Xm/TextF.h>
++#include <stdlib.h>
  #include "externals.h"
  #include <string.h>
-+#include <stdlib.h>
  
- #include <Xm/DragC.h>
- #include <Dt/Dnd.h>
