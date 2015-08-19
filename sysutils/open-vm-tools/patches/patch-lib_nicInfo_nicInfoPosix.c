769fbc9a8f50d012fe5eba2d2adbbf56784e6e16
OBATA Akio <obache@outlook.com>
2015-08-19 17:45:45 +0900

    Better handling for res_state on NetBSD

--- sysutils/open-vm-tools/patches/patch-lib_nicInfo_nicInfoPosix.c
+++ sysutils/open-vm-tools/patches/patch-lib_nicInfo_nicInfoPosix.c
@@ -11,29 +11,19 @@ $NetBSD$
  # include <sys/sysctl.h>
  # include <ifaddrs.h>
  # include <net/if.h>
-@@ -62,6 +62,12 @@
- #   include <net/if.h>
- #endif
- 
-+#if defined(__NetBSD__)
-+static struct __res_state res;
-+#else
-+#  define res (&_res)
-+#endif
-+
- /*
-  * resolver(3) and IPv6:
-  *
-@@ -137,7 +143,7 @@ static int ReadInterfaceDetails(const st
+@@ -135,9 +135,9 @@
+ static void RecordNetworkAddress(GuestNicV3 *nic, const struct addr *addr);
+ static int ReadInterfaceDetails(const struct intf_entry *entry, void *arg);
  static Bool RecordResolverInfo(NicInfoV3 *nicInfo);
- static void RecordResolverNS(DnsConfigInfo *dnsConfigInfo);
+-static void RecordResolverNS(DnsConfigInfo *dnsConfigInfo);
++static void RecordResolverNS(DnsConfigInfo *dnsConfigInfo, res_state res);
  static Bool RecordRoutingInfo(NicInfoV3 *nicInfo);
 -#if !defined(__FreeBSD__) && !defined(__APPLE__) && !defined(USERWORLD)
 +#if !defined(__FreeBSD__) && !defined(__APPLE__) && !defined(__NetBSD__) && !defined(USERWORLD)
  static int GuestInfoGetIntf(const struct intf_entry *entry, void *arg);
  #endif
  #endif
-@@ -280,7 +286,7 @@ GuestInfoGetNicInfo(NicInfoV3 *nicInfo) 
+@@ -280,7 +280,7 @@ GuestInfoGetNicInfo(NicInfoV3 *nicInfo) 
   *
   ******************************************************************************
   */
@@ -42,55 +32,67 @@ $NetBSD$
  
  char *
  GuestInfoGetPrimaryIP(void)
-@@ -470,8 +476,12 @@ RecordResolverInfo(NicInfoV3 *nicInfo)  
-    DnsConfigInfo *dnsConfigInfo = NULL;
+@@ -471,7 +471,14 @@ RecordResolverInfo(NicInfoV3 *nicInfo)  
     char namebuf[DNSINFO_MAX_ADDRLEN + 1];
     char **s;
--
+ 
 +#if defined(__NetBSD__)
-+   memset(&res, 0, sizeof(res));
-+   if (res_ninit(&res) == -1) {
++   res_state res;
++   res = __res_get_state();
++   if (res == NULL) {
 +#else
++#  define res (&_res)
     if (res_init() == -1) {
 +#endif
        return FALSE;
     }
  
-@@ -492,7 +502,7 @@ RecordResolverInfo(NicInfoV3 *nicInfo)  
+@@ -492,17 +499,17 @@ RecordResolverInfo(NicInfoV3 *nicInfo)  
      */
     dnsConfigInfo->domainName =
        Util_SafeCalloc(1, sizeof *dnsConfigInfo->domainName);
 -   *dnsConfigInfo->domainName = Util_SafeStrdup(_res.defdname);
-+   *dnsConfigInfo->domainName = Util_SafeStrdup(res.defdname);
++   *dnsConfigInfo->domainName = Util_SafeStrdup(res->defdname);
  
     /*
      * Name servers.
-@@ -502,7 +512,7 @@ RecordResolverInfo(NicInfoV3 *nicInfo)  
+     */
+-   RecordResolverNS(dnsConfigInfo);
++   RecordResolverNS(dnsConfigInfo, res);
+ 
     /*
      * Search suffixes.
      */
 -   for (s = _res.dnsrch; *s; s++) {
-+   for (s = res.dnsrch; *s; s++) {
++   for (s = res->dnsrch; *s; s++) {
        DnsHostname *suffix;
  
        /* Check to see if we're going above our limit. See bug 605821. */
-@@ -550,12 +560,12 @@ RecordResolverNS(DnsConfigInfo *dnsConfi
+@@ -543,19 +550,19 @@ fail:
+  */
+ 
+ static void
+-RecordResolverNS(DnsConfigInfo *dnsConfigInfo) // IN
++RecordResolverNS(DnsConfigInfo *dnsConfigInfo, res_state res) // IN
+ {
+    int i;
+ 
  #if defined RESOLVER_IPV6_GETSERVERS
     {
        union res_sockaddr_union *ns;
 -      ns = Util_SafeCalloc(_res.nscount, sizeof *ns);
 -      if (res_getservers(&_res, ns, _res.nscount) != _res.nscount) {
-+      ns = Util_SafeCalloc(res.nscount, sizeof *ns);
-+      if (res_getservers(&res, ns, res.nscount) != res.nscount) {
++      ns = Util_SafeCalloc(res->nscount, sizeof *ns);
++      if (res_getservers(res, ns, res->nscount) != res->nscount) {
           g_warning("%s: res_getservers failed.\n", __func__);
           return;
        }
 -      for (i = 0; i < _res.nscount; i++) {
-+      for (i = 0; i < res.nscount; i++) {
++      for (i = 0; i < res->nscount; i++) {
           struct sockaddr *sa = (struct sockaddr *)&ns[i];
           if (sa->sa_family == AF_INET || sa->sa_family == AF_INET6) {
              TypedIpAddress *ip;
-@@ -847,7 +857,7 @@ RecordRoutingInfo(NicInfoV3 *nicInfo)
+@@ -847,7 +854,7 @@ RecordRoutingInfo(NicInfoV3 *nicInfo)
  }
  #endif                                          // else
  
