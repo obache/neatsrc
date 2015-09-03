3d57339dbd9549d62b00b98522bedcccf3d3ef9c
OBATA Akio <obache@outlook.com>
2015-09-03 17:51:34 +0900

    Remove unwanted patch hunk treating NetBSD same as Linux

--- sysutils/open-vm-tools/patches/patch-vgauth_lib_common.c
+++ sysutils/open-vm-tools/patches/patch-vgauth_lib_common.c
@@ -2,15 +2,6 @@ $NetBSD$
 
 --- vgauth/lib/common.c.orig	2015-06-17 16:14:30.000000000 +0000
 +++ vgauth/lib/common.c
-@@ -302,7 +302,7 @@ VGAuth_InstallClient(VGAuthContext *ctx,
- 
- #ifdef _WIN32
-    return VGAUTH_E_OK;
--#elif defined(linux)
-+#elif defined(linux) || defined(__NetBSD__)
-    {
-    gchar *fileName;
-    gchar *lowAppName;
 @@ -374,7 +374,7 @@ done:
  
     return err;
