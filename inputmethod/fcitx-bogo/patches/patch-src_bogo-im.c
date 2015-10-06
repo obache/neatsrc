3b8466bf544adf0d17ee483b9b113a02c34d95f7
OBATA Akio <obache@outlook.com>
2015-10-06 18:21:29 +0900

    [fcitx-bogo] Add version 0.1.2

---
+++ inputmethod/fcitx-bogo/patches/patch-src_bogo-im.c
@@ -0,0 +1,37 @@
+$NetBSD$
+
+* Fix iconv(3) usage
+
+--- src/bogo-im.c.orig	2014-08-10 06:36:29.000000000 +0000
++++ src/bogo-im.c
+@@ -616,7 +616,7 @@ int Utf32ToUtf8Char(const uint32_t c, ch
+     str[0] = c;
+     str[1] = 0;
+     
+-    iconv_t *conv;
++    iconv_t conv;
+     
+     union {
+         short s;
+@@ -644,7 +644,7 @@ int Utf32ToUtf8Char(const uint32_t c, ch
+ 
+ uint32_t Utf8ToUtf32Char(char *src)
+ {
+-    iconv_t *conv;
++    iconv_t conv;
+ 
+     union {
+         short s;
+@@ -661,9 +661,10 @@ uint32_t Utf8ToUtf32Char(char *src)
+     
+     size_t inLength = UTF8_MAX_LENGTH + 1;
+     size_t outLength = sizeof(uint32_t);
+-    IconvStr buff = (IconvStr) output;
++    char* buff = (char*) output;
++    IconvStr str = (IconvStr) str;
+ 
+-    iconv(conv, &src, &inLength, &buff, &outLength);
++    iconv(conv, &str, &inLength, &buff, &outLength);
+     
+     iconv_close(conv);
+     return output[0];
