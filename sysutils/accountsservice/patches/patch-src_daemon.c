799a187dbf257a99611236fbdf0f1792973f7d37
OBATA Akio <obache@outlook.com>
2015-10-08 17:24:48 +0900

    [accountsservice] Fix to monitor master password file as shadow on NetBSD

--- sysutils/accountsservice/patches/patch-src_daemon.c
+++ sysutils/accountsservice/patches/patch-src_daemon.c
@@ -1,10 +1,23 @@
 $NetBSD$
 
+* Monitor master password file as shadow on NetBSD
 * -f option for userdel(8) is only in Linux
 
 --- src/daemon.c.orig	2014-11-04 03:46:00.000000000 +0000
 +++ src/daemon.c
-@@ -1078,6 +1078,7 @@ daemon_delete_user_authorized_cb (Daemon
+@@ -46,7 +46,11 @@
+ #include "util.h"
+ 
+ #define PATH_PASSWD "/etc/passwd"
++#if defined(__NetBSD__)
++#define PATH_SHADOW "/etc/master.passwd"
++#else
+ #define PATH_SHADOW "/etc/shadow"
++#endif
+ #define PATH_GROUP "/etc/group"
+ #define PATH_GDM_CUSTOM "/etc/gdm/custom.conf"
+ 
+@@ -1078,6 +1082,7 @@ daemon_delete_user_authorized_cb (Daemon
          gchar *filename;
          struct passwd *pwent;
          const gchar *argv[6];
@@ -12,7 +25,7 @@ $NetBSD$
  
          pwent = getpwuid (ud->uid);
  
-@@ -1110,20 +1111,16 @@ daemon_delete_user_authorized_cb (Daemon
+@@ -1110,20 +1115,16 @@ daemon_delete_user_authorized_cb (Daemon
          g_remove (filename);
          g_free (filename);
  
