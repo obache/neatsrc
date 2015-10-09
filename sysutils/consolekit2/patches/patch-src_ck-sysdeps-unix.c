7e440eb04582bfa3c2b930841bf53bda32b81742
OBATA Akio <obache@outlook.com>
2015-10-09 18:54:13 +0900

    [consolekit2] Fix pid detection with ck_get_socket_perr_credentials on NetBSD
    NetBSD>=5 have getpeereid(3), but it only for euid.
    Use getsockopt with LOCAL_PEEREID if available in priority to it.

--- sysutils/consolekit2/patches/patch-src_ck-sysdeps-unix.c
+++ sysutils/consolekit2/patches/patch-src_ck-sysdeps-unix.c
@@ -4,7 +4,7 @@ $NetBSD$
 
 --- src/ck-sysdeps-unix.c.orig	2015-08-09 07:46:18.000000000 +0000
 +++ src/ck-sysdeps-unix.c
-@@ -36,7 +36,7 @@
+@@ -36,10 +36,14 @@
  #include <linux/kd.h>
  #endif
  
@@ -13,7 +13,37 @@ $NetBSD$
  #include <dev/wscons/wsdisplay_usl_io.h>
  #endif
  
-@@ -203,7 +203,7 @@ out:
++#if defined(__NetBSD__)
++#include <sys/un.h>
++#endif
++
+ #ifdef HAVE_SYS_VT_H
+ #include <sys/vt.h>
+ #endif
+@@ -108,6 +112,22 @@ ck_get_socket_peer_credentials   (int   
+         if (ucred != NULL) {
+                 ucred_free (ucred);
+         }
++#elif defined(LOCAL_PEEREID)
++        struct unpcbid cr;
++        socklen_t      cr_len;
++
++        cr_len = sizeof (cr);
++
++        if (getsockopt (socket_fd, 0, LOCAL_PEEREID, &cr, &cr_len) == 0 && cr_len == sizeof (cr)) {
++                pid_read = cr.unp_pid;
++                uid_read = cr.unp_euid;
++                ret = TRUE;
++        } else {
++                g_warning ("Failed to getsockopt() credentials, returned len %d/%d: %s\n",
++                           cr_len,
++                           (int) sizeof (cr),
++                           g_strerror (errno));
++        }
+ #elif defined(HAVE_GETPEEREID)
+ 	gid_t dummy;
+ 
+@@ -203,7 +223,7 @@ out:
  gboolean
  ck_fd_is_a_console (int fd)
  {
@@ -22,7 +52,7 @@ $NetBSD$
          struct vt_stat vts;
  #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined (__DragonFly__)
          int vers;
-@@ -211,7 +211,7 @@ ck_fd_is_a_console (int fd)
+@@ -211,7 +231,7 @@ ck_fd_is_a_console (int fd)
          int  kb_ok;
  
          errno = 0;
@@ -31,7 +61,7 @@ $NetBSD$
          kb_ok = (ioctl (fd, VT_GETSTATE, &vts) == 0);
  #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined (__DragonFly__)
          kb_ok = (ioctl (fd, CONS_GETVERS, &vers) == 0);
-@@ -289,6 +289,13 @@ ck_get_a_console_fd (void)
+@@ -289,6 +309,13 @@ ck_get_a_console_fd (void)
          }
  #endif
  
