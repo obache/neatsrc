da7242d98555e12a67e9dfb1e9cce78c139e69e5
OBATA Akio <obache@outlook.com>
2016-02-03 17:58:41 +0900

    [zeromq] Change to use spec macro to detect thread priority scheduling

---
+++ net/zeromq/patches/patch-src_thread.cpp
@@ -0,0 +1,15 @@
+$NetBSD$
+
+* Use spec macro to detect posix thread priority scheduling
+
+--- src/thread.cpp.orig	2015-11-04 08:55:13.000000000 +0000
++++ src/thread.cpp
+@@ -114,7 +114,7 @@ void zmq::thread_t::stop ()
+ 
+ void zmq::thread_t::setSchedulingParameters(int priority_, int schedulingPolicy_)
+ {
+-#if !defined ZMQ_HAVE_ZOS
++#if defined _POSIX_THREAD_PRIORITY_SCHEDULING
+     int policy = 0;
+     struct sched_param param;
+ 
