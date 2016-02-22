e911eb37e144a54571e5a7ccbe7486d5fc8d9a07
OBATA Akio <obache@outlook.com>
2016-02-22 15:23:20 +0900

    [openpgm] Fix to detect CPU freqency on NetBSD

--- net/openpgm/patches/patch-openpgm_pgm_time.c
+++ net/openpgm/patches/patch-openpgm_pgm_time.c
@@ -13,12 +13,17 @@ $NetBSD$
  #		include <sys/sysctl.h>
  #	elif defined(__sun)
  #		include <kstat.h>
-@@ -441,7 +441,7 @@ pgm_time_init (
- 		if (0 == sysctlbyname ("hw.cpufrequency", &cpufrequency, &len, NULL, 0)) {
- 			tsc_khz = (uint_fast32_t)(cpufrequency / 1000);
+@@ -449,6 +449,13 @@ pgm_time_init (
+ 		if (0 == sysctlbyname ("hw.clockrate", &clockrate, &len, NULL, 0)) {
+ 			tsc_khz = (uint_fast32_t)(clockrate * 1000);
  		}
--#elif defined(__FreeBSD__)
-+#elif defined(__FreeBSD__) || defined(__NetBSD__)
- /* frequency in Mhz */
- 		unsigned long clockrate;
- 		size_t len;
++#elif defined(__NetBSD__)
++		uint64_t clockrate;
++		size_t len;
++		len = sizeof (clockrate);
++		if (0 == sysctlbyname ("machdep.tsc_freq", &clockrate, &len, NULL, 0)) {
++			tsc_khz = (uint_fast32_t)(clockrate / 1000);
++		}
+ #elif defined(KSTAT_DATA_INT32)
+ /* ref: http://developers.sun.com/solaris/articles/kstatc.html */
+ 		kstat_ctl_t* kc;
