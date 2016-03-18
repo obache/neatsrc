6bec8d9e4093cbf4cd8bf84efef71a13123a637a
OBATA Akio <obache@outlook.com>
2016-03-18 15:03:36 +0900

    [expat] Revert unwanted bump of BUILDLINK_{API,ABI}_DEPENDS

--- textproc/expat/buildlink3.mk
+++ textproc/expat/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	expat
 .if !defined(EXPAT_BUILDLINK3_MK)
 EXPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.expat+=	expat>=2.1.1
-BUILDLINK_ABI_DEPENDS.expat+=	expat>=2.1.1
+BUILDLINK_API_DEPENDS.expat+=	expat>=1.95.7
+BUILDLINK_ABI_DEPENDS.expat+=	expat>=2.0.0nb1
 BUILDLINK_PKGSRCDIR.expat?=	../../textproc/expat
 .endif # EXPAT_BUILDLINK3_MK
 
