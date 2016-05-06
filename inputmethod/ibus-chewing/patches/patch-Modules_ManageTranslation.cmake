4e0f60f77ab259a6cbb30563edccc34ad62be565
OBATA Akio <obache@outlook.com>
2016-05-06 15:18:56 +0900

    [ibus-chewing] Update to 1.5.0

--- inputmethod/ibus-chewing/patches/patch-Modules_ManageTranslation.cmake
+++ inputmethod/ibus-chewing/patches/patch-Modules_ManageTranslation.cmake
@@ -1,26 +1,15 @@
 $NetBSD: patch-Modules_ManageTranslation.cmake,v 1.4 2014/12/13 06:33:15 obache Exp $
 
 * PKGLOCALEDIR support
-* `-printf' option of find(1) is not portable.
 
---- Modules/ManageTranslation.cmake.orig	2014-10-28 05:05:31.000000000 +0000
+--- Modules/ManageTranslation.cmake.orig	2016-02-25 14:53:41.000000000 +0000
 +++ Modules/ManageTranslation.cmake
-@@ -353,7 +353,7 @@ FUNCTION(MANAGE_POT_FILE potFile)
+@@ -368,7 +368,7 @@ FUNCTION(MANAGE_POT_FILE_INTERNAL showWa
+     SOURCE_ARCHIVE_CONTENTS_ADD("${potFile}" ${srcs} ${depends})
+     SET(cleanList "${potFile}")
  
- 	IF(NOT "${moDir}" STREQUAL "")
- 	    ## MO file
--	    SET(_moDir  "${DATA_DIR}/locale/${_l}/LC_MESSAGES")
-+	    SET(_moDir  "${LOCALE_DIR}/${_l}/LC_MESSAGES")
- 	    SET(_gmoFile "${moDir}/${_l}.gmo")
- 	    ADD_CUSTOM_COMMAND(OUTPUT ${_gmoFile}
- 		COMMAND ${MSGFMT_EXECUTABLE} 
-@@ -393,8 +393,7 @@ FUNCTION(MANAGE_GETTEXT_LOCALES localeLi
-     ELSE()
- 	## LOCALES is not specified, detect now
- 	EXECUTE_PROCESS(
--	    COMMAND find ${poDir} -name "*.po" -printf "%f\n"
--	    COMMAND sed -e "s/.po//g"
-+	    COMMAND find ${poDir} -name "*.po" -exec basename {} .po \;
- 	    COMMAND sort -u
- 	    COMMAND xargs
- 	    COMMAND sed -e "s/ /;/g"
+-    SET(_moInstallRoot "${DATA_DIR}/locale")
++    SET(_moInstallRoot "${LOCALE_DIR}")
+     ## Not only POT, but also PO and MO as well
+     FOREACH(_l ${MANAGE_TRANSLATION_LOCALES})
+ 	## PO file
