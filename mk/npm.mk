406f4230bcb6c8b1928bc2da747e1b981f921680
OBATA Akio <obache@outlook.com>
2016-08-09 17:40:42 +0900

    Add mk fragment for NPM

---
+++ mk/npm.mk
@@ -0,0 +1,41 @@
+# package installation framework for NPM
+#
+# === Package-settable variables ===
+#
+# NPM_PKGNAME
+#	Package name on NPM
+#
+# NPM_PKGVERSION
+#	Package Version on NPM
+
+.if !defined(NPM_MK)
+NPM_MK=	defined
+
+_VARGROUPS+=		npm
+_PKG_VARS.npm=		NPM_PKGNAME NPM_PKGVERSION
+
+_NPM_PKG_PREFIX=	npm
+_NPM=			${LOCALBASE}/bin/npm
+DEPENDS+=		nodejs-[0-9]*:../../lang/nodejs
+
+WRKSRC?=			${WRKDIR}/package
+USE_LANGUAGES?=	# none
+
+NO_BUILD?=	yes
+
+HOMEPAGE?=		http://www.npnjs.com/package/${NPM_PKGNAME}
+
+PKGNAME?=		${_NPM_PKG_PREFIX}-${NPM_PKGNAME}-${NPM_PKGVERSION}
+DISTFILES_VARS+=	npm
+SITES.npm?=		https://registry.npmjs.org/${NPM_PKGNAME}/-/
+DISTFILES.npm+=		${NPM_PKGNAME}-${NPM_PKGVERSION}.tgz
+
+do-install: npm-install
+
+.PHONY: npm-install
+npm-install:
+	${RUN}cd ${WRKSRC}; \
+	${SETENV} ${INSTALL_ENV} ${MAKE_ENV} ${_NPM} -g \
+		--prefix ${DESTDIR}${PREFIX} install
+
+.endif	# NPM_MK
