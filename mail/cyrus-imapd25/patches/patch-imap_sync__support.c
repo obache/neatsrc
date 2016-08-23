3b4984f3e4a208bf158567aed3d81d7d298d65c0
OBATA Akio <obache@outlook.com>
2016-08-23 18:18:57 +0900

    [cyrus-imapd25] Sync patches with upstream

--- mail/cyrus-imapd25/patches/patch-imap_sync__support.c
+++ mail/cyrus-imapd25/patches/patch-imap_sync__support.c
@@ -2,14 +2,22 @@ $NetBSD$
 
 * this decimal constant is unsigned only in ISO C90
 
---- imap/sync_support.c.orig	2015-05-08 03:49:52.000000000 +0000
+--- imap/sync_support.c.orig	2016-07-22 01:55:57.000000000 +0000
 +++ imap/sync_support.c
-@@ -672,7 +672,7 @@ void sync_decode_quota_limits(/*const*/ 
+@@ -57,6 +57,7 @@
+ #include <errno.h>
+ #include <dirent.h>
+ #include <utime.h>
++#include <limits.h>
+ 
+ #include "global.h"
+ #include "mboxlist.h"
+@@ -672,7 +673,7 @@ void sync_decode_quota_limits(/*const*/ 
  
      /* For backwards compatibility */
      if (dlist_getnum32(kl, "LIMIT", &limit)) {
 -	if (limit == 4294967295)
-+	if (limit == 4294967295U)
++	if (limit == UINT_MAX)
  	    limits[QUOTA_STORAGE] = -1;
  	else
  	    limits[QUOTA_STORAGE] = limit;
