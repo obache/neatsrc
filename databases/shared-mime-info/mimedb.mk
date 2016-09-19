b4f85e3eb0876046f2d1aa2a07794e9ef179933f
OBATA Akio <obache@outlook.com>
2016-09-19 15:59:44 +0900

    [shared-mime-info] Prevent to buildlink just for maintain mimedb

--- databases/shared-mime-info/mimedb.mk
+++ databases/shared-mime-info/mimedb.mk
@@ -13,12 +13,16 @@ SHARED_MIME_INFO_MIMEDB_MK=	# defined
 INSTALL_TEMPLATES+=	${.CURDIR}/../../databases/shared-mime-info/files/install.tmpl
 DEINSTALL_TEMPLATES+=	${.CURDIR}/../../databases/shared-mime-info/files/install.tmpl
 
-FILES_SUBST+=		MIMEDB_PATH="${BUILDLINK_PREFIX.shared-mime-info}/share/mime"
-FILES_SUBST+=		UPDATE_MIMEDB="${BUILDLINK_PREFIX.shared-mime-info}/bin/update-mime-database"
+FILES_SUBST+=		MIMEDB_PATH="${LOCALBASE}/share/mime"
+FILES_SUBST+=		UPDATE_MIMEDB="${LOCALBASE}/bin/update-mime-database"
 
 PRINT_PLIST_AWK+=	/^share\/mime\/packages/ { print; next; }
 PRINT_PLIST_AWK+=	/^share\/mime\// { next; }
 
-.include "../../databases/shared-mime-info/buildlink3.mk"
+USE_TOOLS+=	update-mime-database:run
+TOOLS_CREATE+=	update-mime-database
+TOOLS_SCRIPT.update-mime-database?=\
+	${TEST} "$$1" != "-v" || ${LOCALBASE}/bin/update-mime-database -v
+TOOLS_DEPENDS.update-mime-database?=	shared-mime-info-[0-9]*:../../databases/shared-mime-info
 
 .endif	# SHARED_MIME_INFO_MIMEDB_MK
