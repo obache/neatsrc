ad084b942b2158bf27a49cd1876ba8313e7a8630
OBATA Akio <obache@outlook.com>
2017-01-09 17:42:42 +0900

    [json-glib] Drop nls option
    It is not reasonable to drop NLS support just this package,
    because underlying glib suppot NLS unconditionally.

--- textproc/json-glib/options.mk
+++ textproc/json-glib/options.mk
@@ -1,9 +1,9 @@
 # $NetBSD: options.mk,v 1.4 2014/04/20 09:32:57 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.json-glib
-PKG_SUPPORTED_OPTIONS=	introspection nls
-PKG_SUGGESTED_OPTIONS+=	introspection nls
-PLIST_VARS+=		introspection nls
+PKG_SUPPORTED_OPTIONS=	introspection
+PKG_SUGGESTED_OPTIONS+=	introspection
+PLIST_VARS+=		introspection
 
 .include "../../mk/bsd.options.mk"
 
@@ -15,12 +15,4 @@ CONFIGURE_ARGS+=	--enable-introspection=yes
 PLIST.introspection=	yes
 .else
 CONFIGURE_ARGS+=	--enable-introspection=no
-.endif
-
-.if !empty(PKG_OPTIONS:Mnls)
-.include "../../converters/libiconv/buildlink3.mk"
-.include "../../devel/gettext-lib/buildlink3.mk"
-PLIST.nls=		yes
-.else
-CONFIGURE_ARGS+=	--disable-nls
 .endif
