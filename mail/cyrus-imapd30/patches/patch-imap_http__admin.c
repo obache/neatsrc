b606773422df3aa58094af9d7ae3074e5ae0a389
OBATA Akio <obache@outlook.com>
2017-01-19 13:54:30 +0900

    [cyrus-imapd30] Change to use GCC nested function for compat qsort_r

--- mail/cyrus-imapd30/patches/patch-imap_http__admin.c
+++ mail/cyrus-imapd30/patches/patch-imap_http__admin.c
@@ -2,62 +2,75 @@ $NetBSD$
 
 * preven to support only for GLibc with its qsort_r(3)
 
---- imap/http_admin.c.orig	2016-06-29 02:55:04.000000000 +0000
+--- imap/http_admin.c.orig	2017-01-10 23:14:21.000000000 +0000
 +++ imap/http_admin.c
-@@ -479,7 +479,20 @@ static int add_procinfo(pid_t pid,
+@@ -478,10 +478,56 @@ static int add_procinfo(pid_t pid,
      return 0;
  }
  
 -static int sort_procinfo(const struct proc_info **a, const struct proc_info **b,
-+#if defined __GLIBC__
+-                         const char *key)
++#if defined(_GNU_SOURCE) && defined (__GLIBC__) && \
++	((__GLIBC__ > 2) || ((__GLIBC__ == 2) && (__GLIBC_MINOR__ >=0)))
 +#define HAVE_GLIBC_QSORT_R
-+#elif defined(__FreeBSD__) || defined(__DragonFly__) || (defined(__APPLE__) && defined(__MACH__))
++#endif
++
++#if defined(__NEWLIB__) && \
++	((__NEWLIB__ > 2) || ((__NEWLIB__ == 2) && (__NEWLIB_MINOR__ >= 2)))
++#if defined(_GNU_SOURCE)
++#define HAVE_GLIBC_QSORT_R
++#else
++#define HAVE_BSD_QSORT_R
++#endif
++#endif
++
++#if !defined(HAVE_GLIBC_QSORT_R) && \
++	(defined(__FreeBSD__) || defined(__DragonFly__) || defined(__APPLE__))
 +#define HAVE_BSD_QSORT_R
 +#endif
 +
 +#ifdef HAVE_BSD_QSORT_R
-+#define QSORT_COMPARE_ARGS(a,b,c) (c,a,b)
++#define QSORT_R_COMPAR_ARGS(a,b,c) (c,a,b)
++#define cyr_qsort_r(base, nmemb, size, compar, thunk) qsort_r(base, nmemb, size, thunk, compar)
 +#else
-+#define QSORT_COMPARE_ARGS(a,b,c) (a,b,c)
++#define QSORT_R_COMPAR_ARGS(a,b,c) (a,b,c)
++#  if defined(HAVE_GLIBC_QSORT_R)
++#define cyr_qsort_r(base, nmemb, size, compar, thunk) qsort_r(base, nmemb, size, compar, thunk)
++#  elif defined(__GNUC__)
++static void cyr_qsort_r(void *base, size_t nmemb, size_t size,
++                        int (*compar)(const void *, const void *, void *),
++                        void *thunk)
++{
++    int compar_func(const void *a, const void *b)
++    {
++        return compar(a, b, thunk);
++    }
++    qsort(base, nmemb, size, compar_func);
++}
++#  else
++#    error No qsort_r support
++#  endif
 +#endif
 +
-+static int sort_procinfo QSORT_COMPARE_ARGS(
-+                         const struct proc_info **a, const struct proc_info **b,
-                          const char *key)
++static int sort_procinfo QSORT_R_COMPAR_ARGS(
++                         const void *pa, const void *pb,
++                         void *k)
  {
      int r;
-@@ -527,6 +540,16 @@ static int sort_procinfo(const struct pr
-     return (rev ? -r : r);
- }
++    const struct proc_info **a = (const struct proc_info**)pa;
++    const struct proc_info **b = (const struct proc_info**)pb;
++    char *key = (char*)k;
+     int rev = islower((int) *key);
  
-+#if !defined(HAVE_GLIBC_QSORT_R) && !defined(HAVE_BSD_QSORT_R)
-+static __thread char proc_sort_key = 0;
-+
-+static int sort_procinfo_with_key(const struct proc_info **a,
-+                                  const struct proc_info **b)
-+{
-+    return sort_procinfo(a, b, &proc_sort_key);
-+}
-+#endif
-+
- /* Perform a proc action */
- static int action_proc(struct transaction_t *txn)
- {
-@@ -599,8 +622,17 @@ static int action_proc(struct transactio
+     switch (toupper((int) *key)) {
+@@ -598,8 +644,8 @@ static int action_proc(struct transactio
      /* Get and sort info for running processes */
      proc_foreach(add_procinfo, &piarray);
  
-+#if defined(HAVE_GLIBC_QSORT_R)
-     qsort_r(piarray.data, piarray.count, sizeof(struct proc_info *),
-             (int (*)(const void *, const void *, void *)) &sort_procinfo, &key);
-+#elif defined(HAVE_BSD_QSORT_R)
-+    qsort_r(piarray.data, piarray.count, sizeof(struct proc_info *),
-+            &key, (int (*)(const void *, const void *, void *)) &sort_procinfo);
-+#else
-+    proc_sort_key = key;
-+    qsort(piarray.data, piarray.count, sizeof(struct proc_info *),
-+          (int (*)(const void *, const void *)) &sort_procinfo_with_key);
-+#endif
+-    qsort_r(piarray.data, piarray.count, sizeof(struct proc_info *),
+-            (int (*)(const void *, const void *, void *)) &sort_procinfo, &key);
++    cyr_qsort_r(piarray.data, piarray.count, sizeof(struct proc_info *),
++                &sort_procinfo, &key);
  
      /* Send HTML header */
      buf_reset(body);
