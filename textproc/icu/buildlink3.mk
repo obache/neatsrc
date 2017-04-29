41b3134a194c787e6fd779ccebc14587ca4c8cb5
OBATA Akio <obache@outlook.com>
2017-04-29 18:52:16 +0900

    icu: installed header files require c++11 features

--- textproc/icu/buildlink3.mk
+++ textproc/icu/buildlink3.mk
@@ -8,6 +8,11 @@ ICU_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.icu+=	icu>=3.4
 BUILDLINK_ABI_DEPENDS.icu+=	icu>=58.1
 BUILDLINK_PKGSRCDIR.icu?=	../../textproc/icu
+
+.if !empty(USE_LANGUAGES:Mc++)
+USE_LANGUAGES+=	c++11
+.endif
+
 .endif # ICU_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-icu
