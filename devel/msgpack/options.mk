08077eecb878318b052d1b182d2fab9068647f0b
OBATA Akio <obache@outlook.com>
2017-09-21 17:01:47 +0900

    msgpack: Update to 2.1.2

--- devel/msgpack/options.mk
+++ devel/msgpack/options.mk
@@ -7,12 +7,15 @@ PKG_SUPPORTED_OPTIONS=	tests
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mtests)
+BUILDLINK_DEPMETHOD.boost-libs+=	build
+.include "../../devel/boost-libs/buildlink3.mk"
 BUILDLINK_DEPMETHOD.googletest+=	build
 .include "../../devel/googletest/buildlink3.mk"
 BUILDLINK_DEPMETHOD.zlib+=	build
 .include "../../devel/zlib/buildlink3.mk"
 TEST_TARGET=	test
 CMAKE_ARGS+=	-DMSGPACK_BUILD_TESTS:BOOL=ON
+CMAKE_ARGS+=	-DMSGPACK_BOOST:BOOL=ON
 TEST_ENV+=	LD_LIBRARY_PATH=${WRKSRC}:${BUILDLINK_PREFIX.googletest}/lib
 CXXFLAGS+=	${"${CC_VERSION:Mgcc*}" != "":?-Wno-conversion-null:}
 .else
