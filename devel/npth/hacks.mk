012963a4feb35b7fbfa872df67762b4b339ae217
OBATA Akio <obache@outlook.com>
2017-09-30 15:58:33 +0900

    npth: Add a hack for unwanted unwrap

---
+++ devel/npth/hacks.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+SUBST_CLASSES+=		rs_sysp
+SUBST_STAGE.rs_sysp=	subst-unwrap
+SUBST_MESSAGE.rs_sysp=	preserving syspath in npth-config
+SUBST_FILES.rs_sysp=	npth-config
+SUBST_SED.rs_sysp+=	-e 's,-I\/usr\/include|-I\/include),@SYS_INC_OR@),g'
+SUBST_SED.rs_sysp+=	-e 's,-L\/usr\/lib|-L\/lib),@SYS_LIB_OR@),g'
+
+SUBST_CLASSES+=		rp_sysp
+SUBST_STAGE.rp_sysp=	post-build
+SUBST_FILES.rp_sysp=	npth-config
+SUBST_MESSAGE.rp_sysp=	putting syspath in npth-config
+SUBST_SED.rp_sysp=	-e 's,@SYS_INC_OR@,${COMPILER_INCLUDE_DIRS:S/^/-I/g:tW:S/ /|/g},g'
+SUBST_SED.rp_sysp+=	-e 's,@SYS_LIB_OR@,${COMPILER_LIB_DIRS:S/^/-L/g:tW:S/ /|/g},g'
+
+subst-rp_sysp:	subst-unwrap
