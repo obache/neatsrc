e5e5f18491d2eb364875105c9cd9874a864e93be
OBATA Akio <obache@outlook.com>
2017-10-09 14:08:23 +0900

    libchewing: Bump minimum API to 0.5.0 for consistency

--- inputmethod/libchewing/buildlink3.mk
+++ inputmethod/libchewing/buildlink3.mk
@@ -5,8 +5,7 @@ BUILDLINK_TREE+=	libchewing
 .if !defined(LIBCHEWING_BUILDLINK3_MK)
 LIBCHEWING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libchewing+=		libchewing>=0.3.2
-BUILDLINK_ABI_DEPENDS.libchewing+=	libchewing>=0.4.0nb7
+BUILDLINK_API_DEPENDS.libchewing+=		libchewing>=0.5.0
 BUILDLINK_PKGSRCDIR.libchewing?=	../../inputmethod/libchewing
 
 .include "../../databases/sqlite3/buildlink3.mk"
