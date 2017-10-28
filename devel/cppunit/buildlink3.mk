dee4e68b6babfcb3479e74190043b8ce5aab55a0
OBATA Akio <obache@outlook.com>
2017-10-28 17:03:40 +0900

    cppunit: drop unwanted include path addition
    It will break include file namespace separation.

--- devel/cppunit/buildlink3.mk
+++ devel/cppunit/buildlink3.mk
@@ -8,7 +8,6 @@ CPPUNIT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.cppunit+=	cppunit>=1.10.2
 BUILDLINK_ABI_DEPENDS.cppunit+=	cppunit>=1.10.2nb5
 BUILDLINK_PKGSRCDIR.cppunit?=	../../devel/cppunit
-BUILDLINK_CPPFLAGS.cppunit+=	-I${BUILDLINK_PREFIX.cppunit}/include/cppunit
 .endif # CPPUNIT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cppunit
