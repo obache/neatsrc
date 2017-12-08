88494929d2cca3e95a915efe0b8f7be234ead183
OBATA Akio <obache@outlook.com>
2017-12-08 15:37:37 +0900

    librime: remove hidden dependencies form bl3.mk

--- inputmethod/librime/buildlink3.mk
+++ inputmethod/librime/buildlink3.mk
@@ -10,12 +10,6 @@ BUILDLINK_API_DEPENDS.librime+=	librime>=1.0
 BUILDLINK_ABI_DEPENDS.librime?=	librime>=1.0nb6
 BUILDLINK_PKGSRCDIR.librime?=	../../inputmethod/librime
 
-.include "../../converters/opencc/buildlink3.mk"
-.include "../../databases/kyotocabinet/buildlink3.mk"
-.include "../../devel/boost-libs/buildlink3.mk"
-.include "../../devel/google-glog/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../textproc/yaml-cpp/buildlink3.mk"
 .endif	# LIBRIME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-librime
