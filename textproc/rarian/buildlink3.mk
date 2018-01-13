4fb032c0ea3d39e32c7207c1f238719a5b9bb670
OBATA Akio <obache@outlook.com>
2018-01-13 14:09:51 +0900

    rarian: simplify ignored fake tools with TOOLS_NOOP

--- textproc/rarian/buildlink3.mk
+++ textproc/rarian/buildlink3.mk
@@ -8,25 +8,11 @@ RARIAN_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.rarian+=	rarian>=0.6.0
 BUILDLINK_PKGSRCDIR.rarian?=	../../textproc/rarian
 
-BUILDLINK_TARGETS+=	rarian-buildlink-fake
+TOOLS_NOOP+=	scrollkeeper-update
+TOOLS_NOOP+=	scrollkeeper-rebuilddb
+TOOLS_NOOP+=	rarian-sk-update
+TOOLS_NOOP+=	rarian-sk-rebuild
 
-rarian-buildlink-fake:
-	${_PKG_SILENT}${_PKG_DEBUG}					\
-	for fake in scrollkeeper-update	scrollkeeper-rebuilddb		\
-	    rarian-sk-update rarian-sk-rebuild;				\
-	do								\
-		if [ ! -f ${BUILDLINK_DIR}/bin/$${fake} ]; then		\
-			${ECHO_BUILDLINK_MSG}				\
-			    "Creating ${BUILDLINK_DIR}/bin/$${fake}";	\
-			${MKDIR} ${BUILDLINK_DIR}/bin;			\
-			${ECHO} "#!/bin/sh"				\
-			    > ${BUILDLINK_DIR}/bin/$${fake};		\
-			${ECHO} "echo '[pkgsrc] Doing nothing for"	\
-			    "$${fake}' \$$*"				\
-			    >>${BUILDLINK_DIR}/bin/$${fake};		\
-			${CHMOD} +x ${BUILDLINK_DIR}/bin/$${fake};	\
-		fi;							\
-	done
 .endif # RARIAN_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-rarian
