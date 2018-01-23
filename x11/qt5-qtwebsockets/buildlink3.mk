ab8d23d90aa7fbeca1f29c784035996fda4fc6c9
OBATA Akio <obache@outlook.com>
2018-01-23 14:05:38 +0900

    qt5-qtwebsocket: fix buildlink3 guard variable name to match PKGNAME

--- x11/qt5-qtwebsockets/buildlink3.mk
+++ x11/qt5-qtwebsockets/buildlink3.mk
@@ -2,14 +2,14 @@
 
 BUILDLINK_TREE+=	qt5-qtwebsockets
 
-.if !defined(QT5_WEBSOCKETS_BUILDLINK3_MK)
-QT5_WEBSOCKETS_BUILDLINK3_MK:=
+.if !defined(QT5_QTWEBSOCKETS_BUILDLINK3_MK)
+QT5_QTWEBSOCKETS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtwebsockets+=	qt5-qtwebsockets>=5.9.1
 BUILDLINK_ABI_DEPENDS.qt5-qtwebsockets+=	qt5-qtwebsockets>=5.9.1
 BUILDLINK_PKGSRCDIR.qt5-qtwebsockets?=		../../x11/qt5-qtwebsockets
 
 .include "../../x11/qt5-qtdeclarative/buildlink3.mk"
-.endif	# QT5_WEBSOCKETS_BUILDLINK3_MK
+.endif	# QT5_QTWEBSOCKETS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-qt5-qtwebsockets
