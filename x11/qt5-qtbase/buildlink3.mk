343f0e38ab5210a200c9738ce4e041c577cb78f5
OBATA Akio <obache@outlook.com>
2018-03-14 18:30:55 +0900

    x11/qt5-qtbase: stop to add linker assert hack globally
    It should be handled by each packages individually.

--- x11/qt5-qtbase/buildlink3.mk
+++ x11/qt5-qtbase/buildlink3.mk
@@ -21,12 +21,6 @@ MAKE_ENV+=	QTDIR=${QTDIR}
 
 PTHREAD_OPTS+=	require
 
-# linking -lQt5Widgets on NetBSD with gcc produces
-# ld: BFD (NetBSD Binutils nb1) 2.27 assertion fail
-# /usr/src/external/gpl3/binutils/lib/libbfd/../../dist/bfd/elflink.c:2694
-# unless -lexecinfo preceeds -lQt5Widgets
-BUILDLINK_TRANSFORM.NetBSD+=l:Qt5Widgets:execinfo:Qt5Widgets
-
 .include "../../mk/bsd.fast.prefs.mk"
 
 .include "../../converters/libiconv/buildlink3.mk"
