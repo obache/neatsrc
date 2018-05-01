3314ff8e31089097001585f08190bd7557ffca81
OBATA Akio <obache@outlook.com>
2018-05-01 17:47:12 +0900

    inputmethod/librime: change logging to optional

--- inputmethod/librime/options.mk
+++ inputmethod/librime/options.mk
@@ -2,9 +2,17 @@
 #
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.librime
-PKG_SUPPORTED_OPTIONS=		tests
+PKG_SUPPORTED_OPTIONS=		logging tests
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlogging)
+.include "../../devel/google-glog/buildlink3.mk"
+CMAKE_ARGS+=	-DENABLE_LOGGING:BOOL=ON
+TEST_TARGET=	test
+.else
+CMAKE_ARGS+=	-DENABLE_LOGGING:BOOL=OFF
+.endif
 
 .if !empty(PKG_OPTIONS:Mtests)
 .include "../../devel/googletest/buildlink3.mk"
