f65ba1e34d1417c55dc0d94badc144bb29278c35
OBATA Akio <obache@outlook.com>
2018-07-02 14:27:24 +0900

    groonga: change a patch to upstream way

--- textproc/groonga/patches/patch-lib_grn__msgpack.h
+++ textproc/groonga/patches/patch-lib_grn__msgpack.h
@@ -1,20 +1,25 @@
 $NetBSD$
 
-* only required for Windows
+* Suppress a waring with -fstack-protector
 
 --- lib/grn_msgpack.h.orig	2018-06-25 02:46:51.000000000 +0000
 +++ lib/grn_msgpack.h
-@@ -21,9 +21,13 @@
+@@ -21,9 +21,16 @@
  #include "grn.h"
  
  #ifdef GRN_WITH_MESSAGE_PACK
-+# ifdef WIN32
- # define memcpy(dest, src, n) grn_memcpy(dest, src, n)
-+# endif
+-# define memcpy(dest, src, n) grn_memcpy(dest, src, n)
++# ifdef memcpy
++#  define GRN_WITH_CUSTOM_MEMCPY
++# endif /* memcpy */
++# ifndef GRN_WITH_CUSTOM_MEMCPY
++#  define memcpy(dest, src, n) grn_memcpy(dest, src, n)
++# endif /* GRN_WITH_CUSTOM_MEMCPY */
  # include <msgpack.h>
-+# ifdef WIN32
- # undef memcpy
-+# endif
+-# undef memcpy
++# ifdef GRN_WITH_CUSTOM_MEMCPY
++#  undef memcpy
++# endif /* GRN_WITH_CUSTOM_MEMCPY */
  
  # if MSGPACK_VERSION_MAJOR < 1
  typedef unsigned int msgpack_size_t;
