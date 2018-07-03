f011b984992853c563f3d773523d65771fe52a58
OBATA Akio <obache@outlook.com>
2018-07-03 15:25:21 +0900

    llvm: prevent to use $ORIGIN in RPATH.

--- lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
+++ lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
@@ -3,9 +3,9 @@ $NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.3 2017/03/19 19:01:48 adam Exp $
 Don't use non-portable -z discard-unused on SunOS.
 On Darwin, use correct install-name for shared libraries.
 
---- cmake/modules/AddLLVM.cmake.orig	2017-01-17 21:47:58.000000000 +0000
+--- cmake/modules/AddLLVM.cmake.orig	2017-07-12 21:43:14.000000000 +0000
 +++ cmake/modules/AddLLVM.cmake
-@@ -182,9 +182,6 @@ function(add_link_opts target_name)
+@@ -198,9 +198,6 @@ function(add_link_opts target_name)
          # ld64's implementation of -dead_strip breaks tools that use plugins.
          set_property(TARGET ${target_name} APPEND_STRING PROPERTY
                       LINK_FLAGS " -Wl,-dead_strip")
@@ -15,7 +15,7 @@ On Darwin, use correct install-name for shared libraries.
        elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD)
          # Object files are compiled with -ffunction-data-sections.
          # Versions of bfd ld < 2.23.1 have a bug in --gc-sections that breaks
-@@ -1419,7 +1416,7 @@ function(llvm_setup_rpath name)
+@@ -1451,10 +1448,10 @@ function(llvm_setup_rpath name)
    endif()
  
    if (APPLE)
@@ -23,4 +23,8 @@ On Darwin, use correct install-name for shared libraries.
 +    set(_install_name_dir INSTALL_NAME_DIR "${CMAKE_INSTALL_PREFIX}/lib")
      set(_install_rpath "@loader_path/../lib" ${extra_libdir})
    elseif(UNIX)
-     set(_install_rpath "\$ORIGIN/../lib${LLVM_LIBDIR_SUFFIX}" ${extra_libdir})
+-    set(_install_rpath "\$ORIGIN/../lib${LLVM_LIBDIR_SUFFIX}" ${extra_libdir})
++    set(_install_rpath "${CMAKE_INSTALL_PREFIX}/lib${LLVM_LIBDIR_SUFFIX}" ${extra_libdir})
+     if(${CMAKE_SYSTEM_NAME} MATCHES "(FreeBSD|DragonFly)")
+       set_property(TARGET ${name} APPEND_STRING PROPERTY
+                    LINK_FLAGS " -Wl,-z,origin ")
