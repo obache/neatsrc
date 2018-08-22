e995e24b9753fb09d34f61b3435c84620ef84160
OBATA Akio <obache@outlook.com>
2018-08-22 17:33:49 +0900

    motif :update to 2.3.8

--- x11/motif/patches/patch-lib_Xm_XmP.h
+++ x11/motif/patches/patch-lib_Xm_XmP.h
@@ -3,20 +3,14 @@ $NetBSD: patch-lib_Xm_XmP.h,v 1.1 2013/09/08 07:23:56 ryoon Exp $
 * Fix build under OS X 10.8.4 with clang.
   http://bugs.motifzone.net/show_bug.cgi?id=1625
 
---- lib/Xm/XmP.h.orig	2017-03-27 22:24:31.000000000 +0000
+--- lib/Xm/XmP.h.orig	2017-08-28 02:33:10.000000000 +0000
 +++ lib/Xm/XmP.h
-@@ -1437,9 +1437,13 @@ extern void _XmDestroyParentCallback( 
+@@ -1437,7 +1437,7 @@ extern void _XmDestroyParentCallback( 
  
  #endif /* NO_XM_1_2_BC */
  
 -#if __GNUC__
 +#ifdef __GNUC__
  #  define XM_DEPRECATED  __attribute__((__deprecated__))
-+# ifndef __APPLE__
- #  define XM_ALIAS(sym)  __attribute__((__weak__,alias(#sym)))
-+# else
-+#  define XM_ALIAS(sym)
-+# endif
- #else
- #  define XM_DEPRECATED
- #  define XM_ALIAS(sym)
+ #  ifdef NO_WEAK_ALIASES
+ #    define XM_ALIAS(sym)
