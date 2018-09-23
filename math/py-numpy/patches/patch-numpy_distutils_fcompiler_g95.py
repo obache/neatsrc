9967313fecd7fa280031a99408f598ed54ca8c07
OBATA Akio <obache@outlook.com>
2018-09-23 12:08:28 +0900

    py-numpy: tell rpath compiler option for g95

--- math/py-numpy/patches/patch-numpy_distutils_fcompiler_g95.py
+++ math/py-numpy/patches/patch-numpy_distutils_fcompiler_g95.py
@@ -2,9 +2,17 @@ $NetBSD: patch-numpy_distutils_fcompiler_g95.py,v 1.3 2015/04/17 00:41:38 wen Ex
 
 Band aid to make sure that pic_flags is actually used.
 
---- numpy/distutils/fcompiler/g95.py.orig	2014-10-26 14:36:14.000000000 +0000
+--- numpy/distutils/fcompiler/g95.py.orig	2018-08-19 02:17:10.000000000 +0000
 +++ numpy/distutils/fcompiler/g95.py
-@@ -19,9 +19,9 @@ class G95FCompiler(FCompiler):
+@@ -1,6 +1,7 @@
+ # http://g95.sourceforge.net/
+ from __future__ import division, absolute_import, print_function
+ 
++import sys
+ from numpy.distutils.fcompiler import FCompiler
+ 
+ compilers = ['G95FCompiler']
+@@ -19,9 +20,9 @@ class G95FCompiler(FCompiler):
  
      executables = {
          'version_cmd'  : ["<F90>", "--version"],
@@ -17,3 +25,14 @@ Band aid to make sure that pic_flags is actually used.
          'linker_so'    : ["<F90>", "-shared"],
          'archiver'     : ["ar", "-cr"],
          'ranlib'       : ["ranlib"]
+@@ -37,6 +38,10 @@ class G95FCompiler(FCompiler):
+     def get_flags_debug(self):
+         return ['-g']
+ 
++    def runtime_library_dir_option(self, dir):
++        sep = ',' if sys.platform == 'darwin' else '='
++        return '-Wl,-rpath%s"%s"' % (sep, dir)
++
+ if __name__ == '__main__':
+     from distutils import log
+     from numpy.distutils import customized_fcompiler
