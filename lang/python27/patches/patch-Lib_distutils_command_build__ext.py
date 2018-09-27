4be56d74269e75dca61047432078791553223100
OBATA Akio <obache@outlook.com>
2018-09-27 17:05:44 +0900

    python27: move libpython related flags to required place

--- lang/python27/patches/patch-Lib_distutils_command_build__ext.py
+++ lang/python27/patches/patch-Lib_distutils_command_build__ext.py
@@ -1,8 +1,16 @@
 $NetBSD: patch-Lib_distutils_command_build__ext.py,v 1.1 2018/06/17 19:21:21 adam Exp $
 
---- Lib/distutils/command/build_ext.py.orig	2014-12-10 15:59:34.000000000 +0000
+--- Lib/distutils/command/build_ext.py.orig	2018-04-29 22:47:33.000000000 +0000
 +++ Lib/distutils/command/build_ext.py
-@@ -511,8 +511,19 @@ class build_ext (Command):
+@@ -244,6 +244,7 @@ class build_ext (Command):
+             else:
+                 # building python standard extensions
+                 self.library_dirs.append('.')
++            self.rpath.append(sysconfig.get_config_var('LIBDIR'))
+ 
+         # The argument parsing will result in self.define being a string, but
+         # it has to be a list of 2-tuples.  All the preprocessor symbols
+@@ -514,8 +515,19 @@ class build_ext (Command):
          # that go into the mix.
          if ext.extra_objects:
              objects.extend(ext.extra_objects)
