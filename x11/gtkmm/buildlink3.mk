ea4682db88c5f5db2096383fbe103cf08e1d0753
OBATA Akio <obache@outlook.com>
2018-10-03 21:09:51 +0900

    gtkmm: fix to set BUILDLINK_INCDIRS

--- x11/gtkmm/buildlink3.mk
+++ x11/gtkmm/buildlink3.mk
@@ -9,6 +9,8 @@ BUILDLINK_API_DEPENDS.gtkmm+=	gtkmm>=2.22.0
 BUILDLINK_ABI_DEPENDS.gtkmm+=	gtkmm>=2.24.5nb4
 BUILDLINK_PKGSRCDIR.gtkmm?=	../../x11/gtkmm
 
+BUILDLINK_INCDIRS.gtkmm=	include/gtkmm-2.4 lib/gtkmm-2.4/include
+
 .include "../../devel/atkmm/buildlink3.mk"
 .include "../../devel/pangomm/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
