12c349eee2255414d2d41b45aaaffe24738f7945
OBATA Akio <obache@outlook.com>
2018-10-03 21:06:21 +0900

    pangomm: fix BUILDLINK_INCDIRS settings

--- devel/pangomm/buildlink3.mk
+++ devel/pangomm/buildlink3.mk
@@ -9,6 +9,8 @@ BUILDLINK_API_DEPENDS.pangomm+=	pangomm>=2.26.2
 BUILDLINK_ABI_DEPENDS.pangomm+=	pangomm>=2.40.1nb4
 BUILDLINK_PKGSRCDIR.pangomm?=	../../devel/pangomm
 
+BUILDLINK_INCDIRS.pangomm=	include/pangomm-1.4 lib/pangomm-1.4/include
+
 .include "../../devel/glibmm/buildlink3.mk"
 .include "../../devel/pango/buildlink3.mk"
 .include "../../graphics/cairomm/buildlink3.mk"
