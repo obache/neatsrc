d10882fad5d1319ee6ffe2433ff8d1bf4d4ee488
OBATA Akio <obache@outlook.com>
2018-10-04 18:42:01 +0900

    x11/qt5-qtwebsockets: update buildlink patterns

--- x11/qt5-qtwebsockets/buildlink3.mk
+++ x11/qt5-qtwebsockets/buildlink3.mk
@@ -9,6 +9,12 @@ BUILDLINK_API_DEPENDS.qt5-qtwebsockets+=	qt5-qtwebsockets>=5.9.1
 BUILDLINK_ABI_DEPENDS.qt5-qtwebsockets+=	qt5-qtwebsockets>=5.11.1nb1
 BUILDLINK_PKGSRCDIR.qt5-qtwebsockets?=		../../x11/qt5-qtwebsockets
 
+BUILDLINK_INCDIRS.qt5-qtwebsockets=		qt5/include
+BUILDLINK_LIBDIRS.qt5-qtwebsockets=		qt5/lib
+BUILDLINK_PC_DIRS.qt5-qtwebsockets=		lib/pkgconfig
+BUILDLINK_CONTENTS_PATTERNS.qt5-qtwebsockets+=	^qt5/mkspecs/
+BUILDLINK_CONTENTS_PATTERNS.qt5-qtwebsockets+=	^qt5/plugins/
+
 .include "../../x11/qt5-qtdeclarative/buildlink3.mk"
 .endif	# QT5_QTWEBSOCKETS_BUILDLINK3_MK
 
