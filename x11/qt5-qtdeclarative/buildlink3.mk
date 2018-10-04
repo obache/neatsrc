d33d5101c0b83db487b84658040279c322040d79
OBATA Akio <obache@outlook.com>
2018-10-04 18:41:37 +0900

    x11/qt5-qtdeclarative: update buildlink patterns

--- x11/qt5-qtdeclarative/buildlink3.mk
+++ x11/qt5-qtdeclarative/buildlink3.mk
@@ -9,9 +9,11 @@ BUILDLINK_API_DEPENDS.qt5-qtdeclarative+=	qt5-qtdeclarative>=5.9.1
 BUILDLINK_ABI_DEPENDS.qt5-qtdeclarative+=	qt5-qtdeclarative>=5.11.1nb1
 BUILDLINK_PKGSRCDIR.qt5-qtdeclarative?=		../../x11/qt5-qtdeclarative
 
+BUILDLINK_BINDIRS.qt5-qtdeclarative+=	qt5/bin
 BUILDLINK_INCDIRS.qt5-qtdeclarative+=	qt5/include
 BUILDLINK_LIBDIRS.qt5-qtdeclarative+=	qt5/lib
-BUILDLINK_LIBDIRS.qt5-qtdeclarative+=	qt5/plugins
+BUILDLINK_CONTENTS_PATTERNS.qt5-qtdeclarative+=	^qt5/mkspecs/
+BUILDLINK_CONTENTS_PATTERNS.qt5-qtdeclarative+=	^qt5/plugins/
 
 .include "../../x11/qt5-qtxmlpatterns/buildlink3.mk"
 .endif	# QT5_QTDECLARATIVE_BUILDLINK3_MK
