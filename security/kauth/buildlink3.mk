b4dd759cad5f9ca8891c541651808e81cf83180b
OBATA Akio <obache@outlook.com>
2018-10-14 16:07:37 +0900

    kauth: fix to add extra buildlink contents

--- security/kauth/buildlink3.mk
+++ security/kauth/buildlink3.mk
@@ -8,6 +8,8 @@ KAUTH_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kauth+=	kauth>=5.19.0
 BUILDLINK_ABI_DEPENDS.kauth?=	kauth>=5.47.0nb2
 BUILDLINK_PKGSRCDIR.kauth?=	../../security/kauth
+BUILDLINK_CONTENTS_PATTERNS.kauth+=	^qt5/mkspecs/
+BUILDLINK_CONTENTS_PATTERNS.kauth+=	^libexec/kauth/
 
 .include "../../devel/kcoreaddons/buildlink3.mk"
 .include "../../security/polkit-qt5/buildlink3.mk"
