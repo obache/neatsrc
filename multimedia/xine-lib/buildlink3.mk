1afabb0b70a0edace32ca2af6402321a8f89c479
OBATA Akio <obache@outlook.com>
2018-10-19 15:57:11 +0900

    xine-lib: update to 1.2.9

--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
 BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.6nb11
 BUILDLINK_PKGSRCDIR.xine-lib?=		../../multimedia/xine-lib
 
-.include "../../multimedia/ffmpeg2/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
 .endif # XINE_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xine-lib
--- multimedia/xine-lib/patches/patch-src-post-planar-eq.c
+++ multimedia/xine-lib/patches/patch-src-post-planar-eq.c
@@ -1,40 +1,24 @@
 $NetBSD: patch-src-post-planar-eq.c,v 1.3 2014/06/27 12:17:22 jperkin Exp $
 
-https://bugs.xine-project.org/show_bug.cgi?id=524
-
 Disable MMX sections on SunOS.
 
---- src/post/planar/eq.c.orig	2013-09-18 10:04:54.000000000 +0000
+--- src/post/planar/eq.c.orig	2018-01-11 12:49:47.000000000 +0000
 +++ src/post/planar/eq.c
 @@ -31,7 +31,7 @@
  #include <pthread.h>
  
  
--#if defined(ARCH_X86) || defined(ARCH_X86_64)
-+#if (defined(ARCH_X86) || defined(ARCH_X86_64)) && !defined(__sun)
- static void process_MMX(unsigned char *dest, int dstride, unsigned char *src, int sstride,
- 		    int w, int h, int brightness, int contrast)
- {
-@@ -138,9 +138,11 @@ typedef struct eq_parameters_s {
-  * description of params struct
-  */
- START_PARAM_DESCR( eq_parameters_t )
--PARAM_ITEM( POST_PARAM_TYPE_INT, brightness, NULL, -100, 100, 0,
-+PARAM_ITEM( eq_parameters_t, 
-+	    POST_PARAM_TYPE_INT, brightness, NULL, -100, 100, 0,
-             "brightness" )
--PARAM_ITEM( POST_PARAM_TYPE_INT, contrast, NULL, -100, 100, 0,
-+PARAM_ITEM( eq_parameters_t, 
-+	    POST_PARAM_TYPE_INT, contrast, NULL, -100, 100, 0,
-             "contrast" )
- END_PARAM_DESCR( param_descr )
+-#if defined(ARCH_X86)
++#if defined(ARCH_X86) && !defined(__sun)
  
-@@ -259,7 +261,7 @@ static post_plugin_t *eq_open_plugin(pos
+ #if defined(ARCH_X86_64)
+ #  define MEM1(reg) "(%"reg")"
+@@ -278,7 +280,7 @@ static post_plugin_t *eq_open_plugin(pos
    }
  
    process = process_C;
--#if defined(ARCH_X86) || defined(ARCH_X86_64)
-+#if (defined(ARCH_X86) || defined(ARCH_X86_64)) && !defined(__sun)
+-#if defined(ARCH_X86)
++#if defined(ARCH_X86) && !defined(__sun)
    if( xine_mm_accel() & MM_ACCEL_X86_MMX )
      process = process_MMX;
  #endif
