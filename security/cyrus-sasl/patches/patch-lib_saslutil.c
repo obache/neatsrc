030aa20fe4203be3d2e919c0d31fc4220ba29fed
OBATA Akio <obache@outlook.com>
2018-11-23 17:54:49 +0900

    cyrus-sasl: update to 2.1.27

--- security/cyrus-sasl/patches/patch-lib_saslutil.c
+++ security/cyrus-sasl/patches/patch-lib_saslutil.c
@@ -2,16 +2,16 @@ $NetBSD$
 
 * Fix 64bit time_t on 32bit arch
 
---- lib/saslutil.c.orig	2012-10-12 14:05:48.000000000 +0000
+--- lib/saslutil.c.orig	2018-10-09 14:36:41.000000000 +0000
 +++ lib/saslutil.c
-@@ -288,9 +288,9 @@ int sasl_mkchal(sasl_conn_t *conn,
+@@ -280,9 +280,9 @@ int sasl_mkchal(sasl_conn_t *conn,
    time(&now);
  
    if (hostflag && conn->serverFQDN)
--    snprintf(buf,maxlen, "<%lu.%lu@%s>", randnum, now, conn->serverFQDN);
-+    snprintf(buf,maxlen, "<%lu.%llu@%s>", randnum, (unsigned long long)now, conn->serverFQDN);
+-    snprintf(buf,maxlen, "<%lu.%lu@%s>", randnum, (unsigned long)now, conn->serverFQDN); /* don't care much about time 32bit overlap */
++    snprintf(buf,maxlen, "<%lu.%llu@%s>", randnum, (unsigned long long)now, conn->serverFQDN); /* don't care much about time 32bit overlap */
    else
--    snprintf(buf,maxlen, "<%lu.%lu>", randnum, now);
+-    snprintf(buf,maxlen, "<%lu.%lu>", randnum, (unsigned long)now);
 +    snprintf(buf,maxlen, "<%lu.%llu>", randnum, (unsigned long long)now);
  
    return (int) strlen(buf);
