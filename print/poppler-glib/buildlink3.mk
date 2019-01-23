5e5cbbc8a083738758c97dcad69ec4893805bd56
OBATA Akio <obache@outlook.com>
2019-01-23 17:50:01 +0900

    poppler-glib: enable introspection

--- print/poppler-glib/buildlink3.mk
+++ print/poppler-glib/buildlink3.mk
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.72.0
 BUILDLINK_PKGSRCDIR.poppler-glib?=	../../print/poppler-glib
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../graphics/cairo/buildlink3.mk"
+.include "../../graphics/cairo-gobject/buildlink3.mk"
 .include "../../print/poppler/buildlink3.mk"
 .endif # POPPLER_GLIB_BUILDLINK3_MK
 
