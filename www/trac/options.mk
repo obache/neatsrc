14e7f472e7921d6dd363d56e84a0210cd6a26faf
OBATA Akio <obache@outlook.com>
2019-02-05 14:50:01 +0900

    trac: update to 1.2.3

--- www/trac/options.mk
+++ www/trac/options.mk
@@ -2,7 +2,7 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.trac
 PKG_OPTIONS_REQUIRED_GROUPS=	db
-PKG_OPTIONS_GROUP.db=	sqlite psycopg2
+PKG_OPTIONS_GROUP.db=	sqlite pgsql mysql
 PKG_SUGGESTED_OPTIONS=	sqlite
 
 .include "../../mk/bsd.options.mk"
@@ -10,11 +10,9 @@ PKG_SUGGESTED_OPTIONS=	sqlite
 .if !empty(PKG_OPTIONS:Msqlite)
 DEPENDS+=	${PYPKGPREFIX}-sqlite3>=0:../../databases/py-sqlite3
 .endif
-# trac 0.12 dropped support for py-PgSQL.  Switching to py-psycopg2 with
-# an existing database that used to be accessed by py-PgSQL worked.
 .if !empty(PKG_OPTIONS:Mpsycopg2)
 DEPENDS+=	${PYPKGPREFIX}-psycopg2>=2:../../databases/py-psycopg2
 .endif
-# Upstream has MySQL support but there are significant caveats:
-# https://trac.edgewall.org/wiki/MySqlDb
-# Anyone is welcome to add a (tested!) mysql option.
+.if !empty(PKG_OPTIONS:Mmysql)
+DEPENDS+=	${PYPKGPREFIX}-mysqldb>=1.2.2:../../databases/py-mysqldb
+.endif
