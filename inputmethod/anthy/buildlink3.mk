2d5a53b4d8b33fd7e973614e4cb27d1ad91792f9
OBATA Akio <obache@outlook.com>
2019-08-25 16:38:21 +0900

    anthy: update to new upstrem version 0.4
    System dictionay is utf-8 by default, additionally, no need to inject
    additional dictionary at build time (since past 8819 release), so removed
    dictionay options.

--- inputmethod/anthy/buildlink3.mk
+++ inputmethod/anthy/buildlink3.mk
@@ -5,8 +5,7 @@ BUILDLINK_TREE+=	anthy
 .if !defined(ANTHY_BUILDLINK3_MK)
 ANTHY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.anthy+=	anthy>=6300
-BUILDLINK_ABI_DEPENDS.anthy+=	anthy>=7900
+BUILDLINK_API_DEPENDS.anthy+=	anthy>=0.4
 BUILDLINK_PKGSRCDIR.anthy?=	../../inputmethod/anthy
 .endif # ANTHY_BUILDLINK3_MK
 
