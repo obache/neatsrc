07be6b325edd1660defb6959d3fc16a92e1ee029
OBATA Akio <obache@outlook.com>
2019-10-10 17:28:28 +0900

    libhangul: bump minimal API to 0.1.0 for consistency

--- inputmethod/libhangul/buildlink3.mk
+++ inputmethod/libhangul/buildlink3.mk
@@ -5,8 +5,7 @@ BUILDLINK_TREE+=	libhangul
 .if !defined(LIBHANGUL_BUILDLINK3_MK)
 LIBHANGUL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libhangul+=	libhangul>=0.0.4
-BUILDLINK_ABI_DEPENDS.libhangul+=	libhangul>=0.1.0
+BUILDLINK_API_DEPENDS.libhangul+=	libhangul>=0.1.0
 BUILDLINK_PKGSRCDIR.libhangul?=		../../inputmethod/libhangul
 
 .include "../../devel/gettext-lib/buildlink3.mk"
