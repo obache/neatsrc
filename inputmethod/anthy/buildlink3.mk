67e5aa878f6b6b94a80099013f1b09972c11eb2c
OBATA Akio <obache@outlook.com>
2019-12-30 17:05:25 +0900

    anthy: add knob for prepending to legacy version

--- inputmethod/anthy/buildlink3.mk
+++ inputmethod/anthy/buildlink3.mk
@@ -7,6 +7,16 @@ ANTHY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.anthy+=	anthy>=0.4
 BUILDLINK_PKGSRCDIR.anthy?=	../../inputmethod/anthy
+
+.  if !empty(USE_LEGACY_ANTHY_VERSION_SCHEME:M[yY][eE][sS])
+SUBST_CLASSES+=				legacy-anthy-version
+SUBST_STAGE.legacy-anthy-version=	post-wrapper
+SUBST_MESSAGE.legacy-anthy-version=	\
+	Use legacy anthy version scheme for pkg-config
+SUBST_FILES.legacy-anthy-version=	${BUILDLINK_DIR}/lib/pkgconfig/anthy.pc
+SUBST_SED.legacy-anthy-version=		-e 's/^Version: /Version: 9200./'
+.  endif
+
 .endif # ANTHY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-anthy
