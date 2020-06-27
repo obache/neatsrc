a48fd9ac722de452f9084de4a53da528fed7af2c
OBATA Akio <obache@outlook.com>
2020-06-27 14:27:04 +0900

    qt5-qtquickcontrols2: fix buildlink dir paths

--- x11/qt5-qtquickcontrols2/buildlink3.mk
+++ x11/qt5-qtquickcontrols2/buildlink3.mk
@@ -9,6 +9,9 @@ BUILDLINK_API_DEPENDS.qt5-qtquickcontrols2+=	qt5-qtquickcontrols2>=5.10.0
 BUILDLINK_ABI_DEPENDS.qt5-qtquickcontrols2?=	qt5-qtquickcontrols2>=5.13.1
 BUILDLINK_PKGSRCDIR.qt5-qtquickcontrols2?=	../../x11/qt5-qtquickcontrols2
 
+BUILDLINK_INCDIRS.qt5-qtquickcontrols2+=	qt5/include
+BUILDLINK_LIBDIRS.qt5-qtquickcontrols2+=	qt5/lib
+
 .include "../../x11/qt5-qtdeclarative/buildlink3.mk"
 .endif	# QT5_QTQUICKCONTROLS2_BUILDLINK3_MK
 
