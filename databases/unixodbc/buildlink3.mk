947bbc7635364ce22e48782d77a7da6506f6eb03
OBATA Akio <obache@outlook.com>
2020-07-10 16:49:58 +0900

    unixodbc: fix missing libltdl in buildlink3

--- databases/unixodbc/buildlink3.mk
+++ databases/unixodbc/buildlink3.mk
@@ -8,6 +8,8 @@ UNIXODBC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.unixodbc+=	unixodbc>=2.3.2
 BUILDLINK_ABI_DEPENDS.unixodbc+=	unixodbc>=2.3.2
 BUILDLINK_PKGSRCDIR.unixodbc?=		../../databases/unixodbc
+
+.include "../../devel/libltdl/buildlink3.mk"
 .endif # UNIXODBC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-unixodbc
