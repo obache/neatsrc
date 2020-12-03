9ec8209034ba280beb6e46b0a6fa47d5f4172ede
OBATA Akio <obache@outlook.com>
2020-12-03 16:04:53 +0900

    libgphoto2: add more libjpeg compatiblitiy fixes from upstream

--- devel/libgphoto2/patches/patch-camlibs_ax203_jpeg__memsrcdest.c
+++ devel/libgphoto2/patches/patch-camlibs_ax203_jpeg__memsrcdest.c
@@ -4,6 +4,17 @@ Fix build with jpeg-9b.
 
 --- camlibs/ax203/jpeg_memsrcdest.c.orig	2012-12-13 08:49:31.000000000 +0000
 +++ camlibs/ax203/jpeg_memsrcdest.c
+@@ -25,8 +25,8 @@
+ #include "jpeg_memsrcdest.h"
+ 
+ /* libjpeg8 and later come with their own (API compatible) memory source
+-   and dest */
+-#if JPEG_LIB_VERSION < 80
++   and dest, and older versions may have it backported */
++#if JPEG_LIB_VERSION < 80 && !defined(MEM_SRCDST_SUPPORTED)
+ 
+ /* Expanded data source object for memory input */
+ 
 @@ -143,7 +143,7 @@ term_source (j_decompress_ptr cinfo)
  */
  
--- devel/libgphoto2/patches/patch-camlibs_ax203_jpeg__memsrcdest.h
+++ devel/libgphoto2/patches/patch-camlibs_ax203_jpeg__memsrcdest.h
@@ -4,12 +4,18 @@ Fix build with jpeg-9b.
 
 --- camlibs/ax203/jpeg_memsrcdest.h.orig	2012-12-13 08:49:31.000000000 +0000
 +++ camlibs/ax203/jpeg_memsrcdest.h
-@@ -1,7 +1,7 @@
+@@ -1,9 +1,13 @@
  #include <jpeglib.h>
  
++#if JPEG_LIB_VERSION < 80 && !defined(MEM_SRCDST_SUPPORTED)
++
  void
 -jpeg_mem_src (j_decompress_ptr cinfo, unsigned char * buffer,
 +jpeg_mem_src (j_decompress_ptr cinfo, const unsigned char * buffer,
  	unsigned long bufsize);
  
  void
+ jpeg_mem_dest (j_compress_ptr cinfo, unsigned char ** outbuffer,
+ 	unsigned long * outsize);
++
++#endif
