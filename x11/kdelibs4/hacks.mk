231c76a624298f4b1fd82876fc4401eb5bcac301
OBATA Akio <obache@outlook.com>
2021-03-21 14:07:31 +0900

    kdelibs4: remove unwanted qt4/mkspecs/default buildlink hack
    It should be already handled in qt4-tools/buildlink3.mk.

--- x11/kdelibs4/hacks.mk
+++ x11/kdelibs4/hacks.mk
@@ -4,7 +4,3 @@
 post-configure:
 	cp ${FILESDIR}/kshareddatacache.cpp ${WRKSRC}/kdecore/util/kshareddatacache.cpp
 .endif
-
-# fix up buildlinks failure to build link tree correctly
-post-wrapper:
-	ln -s ${BUILDLINK_PREFIX.qt4-libs}/qt4/mkspecs/default ${BUILDLINK_DIR}/qt4/mkspecs/default
